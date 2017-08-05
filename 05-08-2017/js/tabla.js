92b5746c2e6ef4ebbb3ac8fafec585735d99d8cb
feniz18 <fenizhc18@gmail.com>
2017-08-05 15:51:24 -0500

    clase 05/08/2017

---
+++ 05-08-2017/js/tabla.js
@@ -0,0 +1,398 @@
+
+
+$(document).change(function(){
+
+	
+var pulso = false;
+
+
+	$("#boton").click(
+
+
+
+			function(){
+
+			if (pulso == true){
+
+
+				alert("por favor clickee en limpiar para hacer el calculo");
+
+
+			}else{
+
+				var valor1 = $("#fila1").val();
+				var valor2 = $("#fila2").val();
+				var valor3 = $("#fila3").val();
+				var valor4 = $("#fila4").val();
+				var valor5 = $("#fila5").val();
+				var valor6 = $("#fila6").val();
+				var valor7 = $("#fila7").val();
+				var valor8 = $("#fila8").val();
+				var valor9 = $("#fila9").val();
+
+			
+
+				if ((valor1 == "") && (valor2=="") && (valor3=="") && (valor4=="")&& (valor5=="")&& (valor6=="")&& (valor7=="") && (valor8=="")&& (valor9=="")) {
+
+
+					alert("Debe digitar por lo menos un valor");
+
+
+				}else if (valor1 != ""){
+
+						i = valor1-1;
+						z = 9;
+
+						while (i!=valor1){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							z--;
+							i--;
+							
+						
+
+						}
+
+					pulso = true;
+
+
+				}else if (valor2 != ""){
+
+						i = valor2-1;
+						z = 1;
+
+						while (i!=valor2){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+				pulso = true;
+
+
+
+				}else if (valor3 != ""){
+
+						i = valor3-1;
+						z = 2;
+
+						while (i!=valor3){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+					pulso = true;
+
+
+
+				}else if (valor4 != ""){
+
+						i = valor4-1;
+						z = 3;
+
+						while (i!=valor4){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+
+					pulso = true;
+
+
+
+
+
+				}else if (valor5 != ""){
+
+						i = valor5-1;
+						z = 4;
+
+						while (i!=valor5){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+					pulso = true;
+
+
+				}else if (valor6 != ""){
+
+						i = valor6-1;
+						z = 5;
+
+						while (i!=valor6){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+					pulso = true;
+
+
+				}else if (valor7 != ""){
+
+						i = valor7-1;
+						z = 6;
+
+						while (i!=valor7){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+				pulso = true;
+
+
+				}else if (valor8 != ""){
+
+						i = valor8-1;
+						z = 7;
+
+						while (i!=valor8){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+				pulso = true;
+
+
+				}else if (valor9 != ""){
+
+						i = valor9-1;
+						z = 8;
+
+						while (i!=valor9){
+
+							
+
+							if (i==0) {
+
+								i = 9;
+
+								continue;
+
+
+							}
+							
+							$("#fila" + z).val(i);
+							i--;
+
+							if((z-1)==0){
+
+
+								z = 10;
+
+							}
+
+							z--;
+		
+
+						}
+
+						pulso = true;
+				}
+
+
+
+			}
+
+		}
+
+
+		);
+
+
+
+		$("#limpiar").click(function(){
+
+
+
+		 		$("#fila1").val("");
+				$("#fila2").val("");
+				$("#fila3").val("");
+				$("#fila4").val("");
+				$("#fila5").val("");
+				$("#fila6").val("");
+				$("#fila7").val("");
+				$("#fila8").val("");
+				$("#fila9").val("");
+
+				pulso =false;
+
+
+
+
+		});
+
+
+
+});
--- 29-07-2017/js/paises.js
+++ 29-07-2017/js/paises.js
@@ -1,6 +1,7 @@
 document.getElementById("colombia").addEventListener("click",colombiaClick);
 document.getElementById("venezuela").addEventListener("click",venezuelaClick);
 document.getElementById("chile").addEventListener("click",chileClick);
+document.getElementById("matriz").addEventListener("click",matrizClick);
 
 
 function colombiaClick (){
@@ -29,6 +30,17 @@ pais = document.getElementById('combos');
 
 
 	pais.innerHTML = '<select id="ciudad" onchange="chile()"><option>Ciudad</option><option>colonia</option><option>republica presidencial</option><option>petorca</option></select><select id="barrio"><option>Seleccione ciudad</option></select>'
+
+
+
+}
+
+function matrizClick(){
+
+pais = document.getElementById('combos');
+
+
+	pais.innerHTML ='<table><tr id="columna"><td><input type="text" id="fila1"/></td><td><input type="text" id="fila2"/></td><td><input type="text" id="fila3"/></td></tr><tr id="columna"><td><input type="text" id="fila4"/></td><td><input type="text" id="fila5"/></td><td><input type="text" id="fila6"/></td></tr><tr id="columna"><td><input type="text" id="fila7"/></td><td><input type="text" id="fila8"/></td><td><input type="text" id="fila9"/></td></tr></table><div id="cam"><button id="boton">calcular</button> </div><button id="limpiar">limpiar</button></div>'
 
 
 
